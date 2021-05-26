f09d2b3bc89d53bc23a58af12a44898fdb03443d
Wagner Vieira <wagnervieira1209@gmail.com>
2021-05-26 06:31:36 -0300

    Mudando classes para Atom Design

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -21,7 +21,7 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
   }
 
-  h1, h2, h3, h5, h6 {
+  h1, h2, h3, h4, h5, h6 {
     line-height: 1.3;
     font-weight: 700;
     letter-spacing: 2px;
