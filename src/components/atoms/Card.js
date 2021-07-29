9a9264e912f051fa2be8df015a1220450969e742
Wagner Vieira <wagnervieira1209@gmail.com>
2021-07-29 20:13:19 -0300

    Add ProductDetails Page

--- src/components/atoms/Card.js
+++ src/components/atoms/Card.js
@@ -90,7 +90,7 @@ CardMediaDescription.propTypes = {
 const StyledCard = styled.div`
     background-color: #fff;
     border-radius: 4px;
-    border: solid 1px rgba(0, 0, 0, 0.125);
+    border: solid 1px ${(props) => props.theme.colors.border};
     overflow: hidden;
 `;
 
